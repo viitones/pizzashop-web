141d7ba339fe8fc12ddac4d4d4e1e6453682d6db
viitones <vitonesmartins@gmail.com>
2024-09-01 15:54:11 -0300

    initial configs

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './main.css'
+
+import { Button } from './components/ui/button'
+
+export function App() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>
+        Enviar
+      </Button>
+    </div>
+  )
+};
