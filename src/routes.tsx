dc3d1fa4a86b7376c890da61261f565c4c47a96a
viitones <vitonesmartins@gmail.com>
2024-09-02 19:46:46 -0300

    error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom'
 
+import { NotFound } from './pages/404'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/app/dashboard/dashboard'
+import { Dashboard } from './pages/app/dashboard/Dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
