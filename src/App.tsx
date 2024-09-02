176ff8824cc23f0db38f6e0fbf566c5f4a960b11
viitones <vitonesmartins@gmail.com>
2024-09-01 22:43:45 -0300

    initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './main.css'
 
-import { Button } from './components/ui/button'
+// import { Button } from './components/ui/button'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './routes'
+
+import { Toaster } from 'sonner'
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Button>
-        Enviar
-      </Button>
-    </div>
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors closeButton />
+      <RouterProvider router={router} />
+    </HelmetProvider>
   )
 };
---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,61 @@
+/* eslint-disable @stylistic/max-len */
+import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { toast } from 'sonner'
+
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
+export function SignIn() {
+  const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>({
+    resolver: zodResolver(signInForm),
+  })
+
+  async function handleSignIn(data: SignInForm) {
+    try {
+      console.log(data)
+
+      await new Promise((resolve) => setTimeout(resolve, 2000))
+
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch {
+      toast.error('Credencias inválidas')
+    }
+  }
+
+  return (
+    <>
+      <Helmet title="Login" />
+      <div className="p-8">
+        <div className="w-[340px] flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className=" text-2xl font-semibold tracking-tighter">Acessar painel</h1>
+            <p className=" text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro</p>
+          </div>
+
+          <form className=" space-y-4" onSubmit={handleSubmit(handleSignIn)}>
+            <div className=" space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" {...register('email')} />
+            </div>
+            <Button disabled={isSubmitting} type="submit" className="w-full">Acessar Painel</Button>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+}
