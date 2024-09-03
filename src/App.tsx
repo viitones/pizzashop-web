70c575845417d770d2eba2feec3ae6ce17350b13
viitones <vitonesmartins@gmail.com>
2024-09-02 21:33:00 -0300

    prepare for the conecction to api

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 
 export function App() {
   return (
@@ -14,7 +16,9 @@ export function App() {
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors closeButton />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
