7157a9f7112ad7a19a665282af870b5bfdad3c6b
viitones <vitonesmartins@gmail.com>
2024-09-03 17:49:51 -0300

    loading state skeleton

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -16,14 +16,18 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
+import { Skeleton } from './ui/skeleton'
 
 export function AccountMenu() {
-  const { data: profile } = useQuery({
+  const { data: profile, isLoading: isLoadingProfile } = useQuery({
     queryKey: ['profile'],
     queryFn: getProfile,
   })
 
-  const { data: manageRestaurant } = useQuery({
+  const {
+    data: manageRestaurant,
+    isLoading: isLoadingManageRestaurant,
+  } = useQuery({
     queryKey: ['managed-restaurant'],
     queryFn: getManageRestaurant,
   })
@@ -35,17 +39,30 @@ export function AccountMenu() {
           variant="outline"
           className="flex select-none items-center gap-2"
         >
-          {manageRestaurant?.name}
+          {isLoadingManageRestaurant
+            ? <Skeleton className="w-40 h-4" />
+            : manageRestaurant?.name}
           <ChevronDown />
         </Button>
       </DropdownMenuTrigger>
 
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>{profile?.name}</span>
-          <span className=" text-xs font-normal text-muted-foreground">
-            {profile?.email}
-          </span>
+          {isLoadingProfile
+            ? (
+              <div className="space-y-1.5">
+                <Skeleton className="h-4 w-32" />
+                <Skeleton className="h-3 w-24" />
+              </div>
+              )
+            : (
+              <>
+                <span>{profile?.name}</span>
+                <span className=" text-xs font-normal text-muted-foreground">
+                  {profile?.email}
+                </span>
+              </>
+              )}
         </DropdownMenuLabel>
 
         <DropdownMenuSeparator />
