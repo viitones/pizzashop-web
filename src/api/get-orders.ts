788f228316500aad66b138f4551e67552de252b1
viitones <vitonesmartins@gmail.com>
2024-09-07 12:58:24 -0300

    filtro dos pedidos

--- src/api/get-orders.ts
+++ src/api/get-orders.ts
@@ -2,6 +2,9 @@ import { api } from '@/lib/axios'
 
 export interface GetOrdersQuery {
   pageIndex?: number | null
+  orderId?: string | null
+  customerName?: string | null
+  status?: string | null
 }
 
 export interface GetOrdersResponse {
@@ -18,10 +21,13 @@ export interface GetOrdersResponse {
     totalCount: number;
   };
 }
-export async function getOrders({ pageIndex }: GetOrdersQuery) {
+export async function getOrders({ pageIndex, customerName, status, orderId }: GetOrdersQuery) {
   const response = await api.get<GetOrdersResponse>('/orders', {
     params: {
       pageIndex,
+      orderId,
+      customerName,
+      status,
     },
   })
 
