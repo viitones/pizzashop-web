231615ea8493b66a0a6cd6aa1bab7e942284d1c8
viitones <vitonesmartins@gmail.com>
2024-09-18 22:54:14 -0300

    primeiros testes unitários

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
-import path from "path"
+/// <reference types="vitest/config" />
+/// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
+import path from 'path'
+
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
+  },
+  test: {
+    globals: true,
+    setupFiles: ['./test/setup.ts'],
+    environment: 'happy-dom',
   },
 })
