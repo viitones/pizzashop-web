323e720585960994616bda018555f93124466f80
viitones <vitonesmartins@gmail.com>
2024-09-25 21:29:00 -0300

    Sign In mock

--- src/api/moks/index.ts
+++ src/api/moks/index.ts
@@ -1,7 +1,8 @@
 import { env } from '@/env'
 import { setupWorker } from 'msw/browser'
+import { signInMock } from './sign-in.mock'
 
-export const worker = setupWorker()
+export const worker = setupWorker(signInMock)
 
 export async function enableMSW() {
   if (env.MODE !== 'test') {
