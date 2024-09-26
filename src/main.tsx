0cbe5f3235eeacd94d74068ae277809d78c07b33
viitones <vitonesmartins@gmail.com>
2024-09-25 21:14:15 -0300

    start config moks from MSW

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { enableMSW } from './api/moks/index.ts'
 import { App } from './app.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+enableMSW().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  )
+})
